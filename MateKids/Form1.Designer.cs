b535074500ad5d7158202d87e3bc33d95740b5bc
Matt210119 <mattgranja99@gmail.com>
2019-12-23 10:22:20 -0500

    Cambios1

--- MateKids/Form1.Designer.cs
+++ MateKids/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.titulo_juego = new System.Windows.Forms.PictureBox();
+<<<<<<< HEAD
             this.titulo_minijuego_8 = new System.Windows.Forms.PictureBox();
             this.icono_minijuego_8 = new System.Windows.Forms.PictureBox();
             this.titulo_minijuego_10 = new System.Windows.Forms.PictureBox();
@@ -68,6 +69,47 @@
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego_2)).BeginInit();
+=======
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.pictureBox18 = new System.Windows.Forms.PictureBox();
+            this.pictureBox19 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.titulo_minijuego_1 = new System.Windows.Forms.PictureBox();
+            this.icono_minijuego_1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.titulo_juego)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+>>>>>>> parent of 44672d4... revision lunes 23 diciembre
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego_1)).BeginInit();
             this.SuspendLayout();
@@ -83,6 +125,7 @@
             this.titulo_juego.TabIndex = 0;
             this.titulo_juego.TabStop = false;
             // 
+<<<<<<< HEAD
             // titulo_minijuego_8
             // 
             this.titulo_minijuego_8.Image = global::MateKids.Properties.Resources.multiplicax_juego;
@@ -262,6 +305,169 @@
             this.icono_minijuego_2.Size = new System.Drawing.Size(207, 164);
             this.icono_minijuego_2.TabIndex = 63;
             this.icono_minijuego_2.TabStop = false;
+=======
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox12.Location = new System.Drawing.Point(546, 440);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 80;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(546, 476);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox13.TabIndex = 79;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox14.Location = new System.Drawing.Point(976, 440);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox14.TabIndex = 78;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(976, 476);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox15.TabIndex = 77;
+            this.pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox16.Location = new System.Drawing.Point(758, 440);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox16.TabIndex = 76;
+            this.pictureBox16.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.Location = new System.Drawing.Point(758, 476);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox17.TabIndex = 75;
+            this.pictureBox17.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            this.pictureBox18.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox18.Location = new System.Drawing.Point(331, 440);
+            this.pictureBox18.Name = "pictureBox18";
+            this.pictureBox18.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox18.TabIndex = 74;
+            this.pictureBox18.TabStop = false;
+            // 
+            // pictureBox19
+            // 
+            this.pictureBox19.Location = new System.Drawing.Point(331, 476);
+            this.pictureBox19.Name = "pictureBox19";
+            this.pictureBox19.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox19.TabIndex = 73;
+            this.pictureBox19.TabStop = false;
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox20.Location = new System.Drawing.Point(113, 440);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox20.TabIndex = 72;
+            this.pictureBox20.TabStop = false;
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.Location = new System.Drawing.Point(113, 476);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox21.TabIndex = 71;
+            this.pictureBox21.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox10.Location = new System.Drawing.Point(546, 201);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 70;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(546, 237);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox11.TabIndex = 69;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox8.Location = new System.Drawing.Point(976, 201);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 68;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(976, 237);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox9.TabIndex = 67;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox6.Location = new System.Drawing.Point(758, 201);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 66;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(758, 237);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox7.TabIndex = 65;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MateKids.Properties.Resources.multiplicax_juego;
+            this.pictureBox4.Location = new System.Drawing.Point(331, 201);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(155, 30);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 64;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(331, 237);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(155, 133);
+            this.pictureBox5.TabIndex = 63;
+            this.pictureBox5.TabStop = false;
+>>>>>>> parent of 44672d4... revision lunes 23 diciembre
             // 
             // titulo_minijuego_1
             // 
@@ -292,6 +498,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< HEAD
             this.ClientSize = new System.Drawing.Size(1668, 820);
             this.Controls.Add(this.titulo_minijuego_8);
             this.Controls.Add(this.icono_minijuego_8);
@@ -311,6 +518,27 @@
             this.Controls.Add(this.icono_minijuego);
             this.Controls.Add(this.titulo_minijuego_2);
             this.Controls.Add(this.icono_minijuego_2);
+=======
+            this.ClientSize = new System.Drawing.Size(1251, 666);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox17);
+            this.Controls.Add(this.pictureBox18);
+            this.Controls.Add(this.pictureBox19);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+>>>>>>> parent of 44672d4... revision lunes 23 diciembre
             this.Controls.Add(this.titulo_minijuego_1);
             this.Controls.Add(this.icono_minijuego_1);
             this.Controls.Add(this.titulo_juego);
@@ -319,6 +547,7 @@
             this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
             this.Text = " ";
             ((System.ComponentModel.ISupportInitialize)(this.titulo_juego)).EndInit();
+<<<<<<< HEAD
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego_8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_10)).EndInit();
@@ -337,6 +566,26 @@
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego_2)).EndInit();
+=======
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+>>>>>>> parent of 44672d4... revision lunes 23 diciembre
             ((System.ComponentModel.ISupportInitialize)(this.titulo_minijuego_1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.icono_minijuego_1)).EndInit();
             this.ResumeLayout(false);
@@ -346,6 +595,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox titulo_juego;
+<<<<<<< HEAD
         private System.Windows.Forms.PictureBox titulo_minijuego_8;
         private System.Windows.Forms.PictureBox icono_minijuego_8;
         private System.Windows.Forms.PictureBox titulo_minijuego_10;
@@ -364,6 +614,26 @@
         private System.Windows.Forms.PictureBox icono_minijuego;
         private System.Windows.Forms.PictureBox titulo_minijuego_2;
         private System.Windows.Forms.PictureBox icono_minijuego_2;
+=======
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.PictureBox pictureBox18;
+        private System.Windows.Forms.PictureBox pictureBox19;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+>>>>>>> parent of 44672d4... revision lunes 23 diciembre
         private System.Windows.Forms.PictureBox titulo_minijuego_1;
         private System.Windows.Forms.PictureBox icono_minijuego_1;
     }
